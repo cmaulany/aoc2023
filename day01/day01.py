e85b557dac1b3ff0e49b4637fadcf2bc526d16e1
Christian Maulany <christian@vilmco.nl>
2023-12-01 14:50:33 +0100

    Separate find_calibration_value function

--- day01/day01.py
+++ day01/day01.py
@@ -1,4 +1,4 @@
-digit_values = {
+number_digits = {
     "0": "0",
     "1": "1",
     "2": "2",
@@ -11,7 +11,7 @@ digit_values = {
     "9": "9",
 }
 
-word_values = {
+word_digits = {
     "one": "1",
     "two": "2",
     "three": "3",
@@ -23,39 +23,39 @@ word_values = {
     "nine": "9",
 }
 
-values_part1 = digit_values
-values_part2 = {**digit_values, **word_values}
+valid_digits_part1 = number_digits
+valid_digits_part2 = {**number_digits, **word_digits}
 
 
 def parse_input(stream):
     return stream.readlines()
 
 
-def solve(input, values):
-    numbers = []
-    for line in input:
-        first_pairs = [
-            (line.find(key), digit) for key, digit in values.items() if key in line
-        ]
-        min_pair = min(first_pairs, key=lambda pair: pair[0])
-        first_digit = min_pair[1]
+def find_calibration_value(line, valid_digits):
+    first_pairs = [
+        (line.find(key), digit) for key, digit in valid_digits.items() if key in line
+    ]
+    min_pair = min(first_pairs, key=lambda pair: pair[0])
+    first_digit = min_pair[1]
 
-        last_pairs = [
-            (line.rfind(key), digit) for key, digit in values.items() if key in line
-        ]
-        max_pair = max(last_pairs, key=lambda pair: pair[0])
-        last_digit = max_pair[1]
+    last_pairs = [
+        (line.rfind(key), digit) for key, digit in valid_digits.items() if key in line
+    ]
+    max_pair = max(last_pairs, key=lambda pair: pair[0])
+    last_digit = max_pair[1]
 
-        number = int(first_digit + last_digit)
-        numbers.append(number)
+    return int(first_digit + last_digit)
 
-    calibration_value = sum(numbers)
-    return calibration_value
+
+def solve(input, valid_digits):
+    calibration_values = [find_calibration_value(line, valid_digits) for line in input]
+    total = sum(calibration_values)
+    return total
 
 
 def solve_part1(input):
-    return solve(input, values_part1)
+    return solve(input, valid_digits_part1)
 
 
 def solve_part2(input):
-    return solve(input, values_part2)
+    return solve(input, valid_digits_part2)
