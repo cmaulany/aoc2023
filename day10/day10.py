fab2647f6beeedc48e9b5f8f5396112a37207440
Christian Maulany <cmaulany@gmail.com>
2023-12-10 17:05:27 +0100

    tests

--- day10/day10.py
+++ day10/day10.py
@@ -31,11 +31,11 @@ def get_start(map):
 def parse_input(f):
     map = [line.strip() for line in f.readlines()]
 
-    start, start_type = get_start(map)
-    sx, sy = start
+    start_position, start_type = get_start(map)
+    sx, sy = start_position
 
     map[sy] = map[sy][:sx] + start_type + map[sy][sx + 1 :]
-    return map, start
+    return map, start_position
 
 
 def get_neighbors(map, position):
@@ -79,25 +79,23 @@ def find_path(map, start):
 
     path = []
     current = start
-    while not path or current != start:
+    while current != start or not path:
         path.append(current)
         current = prevs[current]
-
     return path
 
 
-def get_sub_neighbors(map, position):
+def get_sub_neighbors(map, start_position):
     """
     We divide each tile into for subtiles:
     A | B
     - + -
     C | D
     """
-    px, py = position
+    x, y = start_position
+    type = map[int(y / 2)][int(x / 2)]
 
-    type = map[int(py / 2)][int(px / 2)]
-
-    sub_position = (px % 2, py % 2)
+    sub_position = (x % 2, y % 2)
     sub_type = {
         (0, 0): "A",
         (1, 0): "B",
@@ -150,14 +148,12 @@ def get_sub_neighbors(map, position):
         },
     }[type][sub_type]
 
-    neighbors = [(px + dx, py + dy) for dx, dy in deltas]
+    neighbors = [(x + dx, y + dy) for dx, dy in deltas]
 
     width = len(map[0]) * 2
     height = len(map) * 2
     return [
-        (px, py)
-        for px, py in neighbors
-        if px >= 0 and px < width and py >= 0 and py < height
+        (x, y) for x, y in neighbors if x >= 0 and x < width and y >= 0 and y < height
     ]
 
 
@@ -172,10 +168,12 @@ def flood_fill(map, position=(0, 0)):
             if neighbor not in visited:
                 open.append(neighbor)
 
+    width = range(len(map[0]))
+    height = range(len(map))
     return {
         (x, y)
-        for y in range(len(map))
-        for x in range(len(map[0]))
+        for x in width
+        for y in height
         if (x * 2, y * 2) in visited
         and (x * 2 + 1, y * 2) in visited
         and (x * 2, y * 2 + 1) in visited
@@ -184,26 +182,27 @@ def flood_fill(map, position=(0, 0)):
 
 
 def solve_part1(input):
-    map, start = input
-    path = find_path(map, start)
+    map, start_position = input
+    path = find_path(map, start_position)
     return len(path) // 2
 
 
 def solve_part2(input):
-    map, start = input
+    map, start_position = input
 
-    loop = set(find_path(map, start))
-
+    loop = set(find_path(map, start_position))
     loop_map = [
         "".join(type if (x, y) in loop else "." for x, type in enumerate(line))
         for y, line in enumerate(map)
     ]
     outer_positions = flood_fill(loop_map)
 
+    width = range(len(map[0]))
+    height = range(len(map))
     enclosed_positions = [
         (x, y)
-        for x in range(len(map[0]))
-        for y in range(len(map))
+        for x in width
+        for y in height
         if (x, y) not in loop and (x, y) not in outer_positions
     ]
     return len(enclosed_positions)
---
+++ day10/test.py
@@ -0,0 +1,36 @@
+import unittest
+from day10 import parse_input, solve_part1, solve_part2
+
+
+class TestDay10(unittest.TestCase):
+    def test_example_input_part1_1(self):
+        with open("example_input_part1_1.txt") as stream:
+            input = parse_input(stream)
+
+        solution = solve_part1(input)
+        self.assertEqual(solution, 4)
+
+    def test_example_input_part1_2(self):
+        with open("example_input_part1_2.txt") as stream:
+            input = parse_input(stream)
+
+        solution = solve_part1(input)
+        self.assertEqual(solution, 8)
+
+    def test_example_input_part2_1(self):
+        with open("example_input_part2_1.txt") as stream:
+            input = parse_input(stream)
+
+        solution = solve_part2(input)
+        self.assertEqual(solution, 4)
+
+    def test_example_input_part2_2(self):
+        with open("example_input_part2_2.txt") as stream:
+            input = parse_input(stream)
+
+        solution = solve_part2(input)
+        self.assertEqual(solution, 10)
+
+
+if __name__ == "__main__":
+    unittest.main()
