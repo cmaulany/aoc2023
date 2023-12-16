4497dca6bbca9caf005cc846bf60de968b7b2fb4
Christian Maulany <cmaulany@gmail.com>
2023-12-16 16:12:24 +0100

    simplified day07

--- day07/day07.py
+++ day07/day07.py
@@ -14,52 +14,46 @@ def score_hand(hand, wildcards=False):
     if wildcards:
         hand_without_wildcards = "".join(c for c in hand if c != "J")
         jacks = hand.count("J")
-        card_order = ["A", "K", "Q", "T", "9", "8", "7", "6", "5", "4", "3", "2", "J"]
+        card_order = "AKQT98765432J"
     else:
         hand_without_wildcards = hand
         jacks = 0
-        card_order = ["A", "K", "Q", "J", "T", "9", "8", "7", "6", "5", "4", "3", "2"]
+        card_order = "AKQJT98765432"
 
     counts = Counter(hand_without_wildcards)
     values = sorted(counts.values(), reverse=True)
 
-    score = pow(13, 14)
     if hand == "JJJJJ" or values[0] + jacks >= 5:
-        # "five of a kind"
-        score *= 6
+        # five of a kind
+        combination_value = 6
     elif values[0] + jacks >= 4:
-        # "four of a kind"
-        score *= 5
+        # four of a kind
+        combination_value = 5
     elif (values[0] + jacks >= 3 and values[1] >= 2) or (
         values[0] == 3 and values[1] + jacks >= 2
     ):
-        # "full house"
-        score *= 4
+        # full house
+        combination_value = 4
     elif values[0] + jacks >= 3:
-        # "three of a kind"
-        score *= 3
+        # three of a kind
+        combination_value = 3
     elif values.count(2) == 2:
-        # "two pair"
-        score *= 2
+        # two pair
+        combination_value = 2
     elif values[0] == 2 or jacks == 1:
-        # "one pair"
-        score *= 1
+        # one pair
+        combination_value = 1
     else:
-        # "high card"
-        score *= 0
+        # high card
+        combination_value = 0
 
-    tie_breaker_score = sum(
-        (12 - card_order.index(c)) * pow(13, (13 - i)) for i, c in enumerate(hand)
-    )
-    score += tie_breaker_score
-    return score
+    hand_value = tuple(len(card_order) - card_order.index(card) for card in hand)
+    return (combination_value, hand_value)
 
 
 def solve(input, wildcards):
-    scores = [(score_hand(hand, wildcards=wildcards), bid) for hand, bid in input]
-    scores.sort()
-
-    total_winnings = sum((i + 1) * bid for i, (_, bid) in enumerate(scores))
+    sorted_input = sorted(input, key=lambda x: score_hand(x[0], wildcards=wildcards))
+    total_winnings = sum((i + 1) * bid for i, (_, bid) in enumerate(sorted_input))
     return total_winnings
 
 
